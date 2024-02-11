f52649a1b0b28bfc353d706693a1f91bca0074cf
Akshay Shrivastava <94282391+akshayyy13@users.noreply.github.com>
2024-02-11 19:27:26 +0530

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer/Footer'
-import Header from './components/Header/header'
+import Header from './components/Header/Header'
 function Layout() {
   return (
     <div>

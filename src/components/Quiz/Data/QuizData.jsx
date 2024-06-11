9835d0a088098fac602fdc766fe1b6e273860d16
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-06-11 17:55:15 +0530

    Update QuizData.jsx

--- src/components/Quiz/Data/QuizData.jsx
+++ src/components/Quiz/Data/QuizData.jsx
@@ -19,4 +19,24 @@ export const QuizData = [
         options: ["1996", "1995", "1994", "none of the above"],
         answer:2
     },
+    {
+        question: "'.MOV' extension refers usually to what kind of file?",
+        options: ["Image file", "Animation/movie file", "Audio file", "MS Office document"],
+        answer:2
+    },
+    {
+        question: "What's a web browser?",
+        options: ["A software program that allows you to access sites on the World Wide Web", "A person who likes to look at websites", "A computer that stores WWW files", "A kind of spider"],
+        answer:2
+    },
+    {
+        question: "How many bits is a byte?",
+        options: ["4", "8", "16", "32"],
+        answer:2
+    },
+    {
+        question: "http://www.indiabix.com - is an example of what?",
+        options: ["A URL", "An access code", "A directory", "A server"],
+        answer:2
+    },
 ];
